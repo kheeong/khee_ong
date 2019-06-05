d775f6f0b882b7ac26ac00a23ec8d394b5ab8cb6
kheeong <ngkheeong08000@gmail.com>
2019-06-05 21:12:06 +0800

    Final message

---
+++ app/src/main/java/com/example/user/v_s/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.user.v_s;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView search_food;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        search_food = (ListView) findViewById(R.id.search_food);
+
+        ArrayList<String> arrayFood = new ArrayList<>();
+        arrayFood.addAll(Arrays.asList(getResources().getStringArray(R.array.my_foods)));
+
+        adapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayFood
+        );
+        search_food.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_food);
+        SearchView searchView = (SearchView) item.getActionView();
+        ListView listView = (ListView) findViewById(R.id.search_food);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,String.valueOf(adapter.getItem(position)),Toast.LENGTH_SHORT).show();
+                if(String.valueOf(adapter.getItem(position)).equals("Ice cream")){
+                    Intent intent = new Intent(MainActivity.this,IceCream.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
+
