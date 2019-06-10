97657c93653fa8915998e6bba59f5121010b36fc
kheeong <ngkheeong08000@gmail.com>
2019-06-10 10:50:26 +0800

    V2

--- app/src/main/java/com/example/user/v_s/Oreo_Ice_Cream.java
+++ app/src/main/java/com/example/user/v_s/Oreo_Ice_Cream.java
@@ -1,6 +1,27 @@
 package com.example.user.v_s;
 
-public class Oreo_Ice_Cream {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
-    
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Oreo_Ice_Cream extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ice_cream);
+
+    }
 }
